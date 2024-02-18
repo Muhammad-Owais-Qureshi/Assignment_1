811adeb12eb0990520ef6425929cd3c4ff19e044
Muhammad-Owais-Qureshi <159189982+Muhammad-Owais-Qureshi@users.noreply.github.com>
2024-02-18 15:12:23 +0500

    Add files via upload

---
+++ Exercise_3/main.ts
@@ -0,0 +1,7 @@
+// Task 4 Famous Quote:
+console.log('Albert Einstein once said, “A person who never made a mistake never tried anything new.”')
+// Task 5 Famous Quote 2:
+let person = 'Albert Einstein';
+let message = 'once said, “A person who never made a mistake never tried anything new.”'
+console.log(person,message);
+
