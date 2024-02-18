7add3b15883abfb21239c8e2f16a04390149ec18
Muhammad-Owais-Qureshi <159189982+Muhammad-Owais-Qureshi@users.noreply.github.com>
2024-02-18 14:51:10 +0500

    Add files via upload

---
+++ Exercise_2/main.js
@@ -0,0 +1,9 @@
+"use strict";
+let Name = 'owAiS';
+let tital = Name.slice(0, 1).toUpperCase();
+let low = Name.slice(1).toLowerCase();
+let con = tital + low;
+console.log(con);
+let Name2 = 'owais QUreSHi';
+console.log(Name2.toLowerCase());
+console.log(Name2.toUpperCase());
