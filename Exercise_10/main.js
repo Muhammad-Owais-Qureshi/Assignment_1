e1747f2d38e4bfca1f9c28a0c19e24a2b702428f
Muhammad-Owais-Qureshi <159189982+Muhammad-Owais-Qureshi@users.noreply.github.com>
2024-02-21 00:06:10 +0500

    Add files via upload

---
+++ Exercise_10/main.js
@@ -0,0 +1,7 @@
+"use strict";
+//Task 15 Changing Guest List:
+let NewGuest = ['Farooq', 'Mudabbir', 'Owais'];
+NewGuest.splice(2, 1, 'Rehan');
+console.log("Hi", NewGuest[0], "i invite you tonight dinner with me");
+console.log("Hi", NewGuest[1], "i invite you tonight dinner with me");
+console.log("Hi", NewGuest[2], "i invite you tonight dinner with me");
