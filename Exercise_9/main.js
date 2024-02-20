c2d56b3f05005780499b7d5a6541c59daad0de16
Muhammad-Owais-Qureshi <159189982+Muhammad-Owais-Qureshi@users.noreply.github.com>
2024-02-20 23:59:57 +0500

    Add files via upload

---
+++ Exercise_9/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// Task 13 Your Own Array:
+let arr = ['Car', 'Bike', 'Bus'];
+console.log('I like Civic', arr[0]);
+console.log('I would like to own a Honda motor', arr[1]);
+console.log('Red', arr[0], 'arrived in karachi');
