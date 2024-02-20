0e9de1b27c6531b2abf3a057963829aeab89ed1f
Muhammad-Owais-Qureshi <159189982+Muhammad-Owais-Qureshi@users.noreply.github.com>
2024-02-20 23:55:47 +0500

    Add files via upload

---
+++ Exercise_7/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// Task Names Arry:
+let Friends = ['Farooq', 'Mudabbir', 'Hunain'];
+console.log(Friends[0]);
+console.log(Friends[1]);
+console.log(Friends[2]);
---
+++ Exercise_7/main.ts
@@ -0,0 +1,5 @@
+// Task Names Arry:
+let Friends = ['Farooq','Mudabbir','Hunain']
+console.log(Friends[0])
+console.log(Friends[1])
+console.log(Friends[2])
