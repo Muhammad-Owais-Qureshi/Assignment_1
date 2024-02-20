9c4a9aec26857e5968b210e38b28dcad907d95b6
Muhammad-Owais-Qureshi <159189982+Muhammad-Owais-Qureshi@users.noreply.github.com>
2024-02-20 23:57:26 +0500

    Add files via upload

---
+++ Exercise_8/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// Task 12 Greetings:
+let friends = ['farooq', 'mudabbir', 'Anus'];
+console.log(friends[0], 'is my best friend');
+console.log(friends[1], 'is my best friend');
+console.log(friends[2], 'is my best friend');
