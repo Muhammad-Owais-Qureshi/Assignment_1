f835955676440288f3a172b7692085d62b250ef0
Muhammad-Owais-Qureshi <159189982+Muhammad-Owais-Qureshi@users.noreply.github.com>
2024-02-21 00:02:31 +0500

    Add files via upload

---
+++ Exercise/main.ts
@@ -0,0 +1,11 @@
+// Task 14 Guest List:
+let Guest = ['Farooq','Mudabbir','Owais']
+console.log( 'Salam', "",Guest[0],' i invite tonight dinner with me')
+console.log( 'Salam', "",Guest[1],' i invite tonight dinner with me')
+console.log( 'Salam', "",Guest[2],' i invite tonight dinner with me')
+console.log(Guest.pop(),'"Sorry you can`t make it"')
+
+// Total Guest invition
+for(let i = 0;i <Guest.length;i++){
+    console.log(Guest[i],'you invite tonight dinner with me')
+}
